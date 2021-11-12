5f0732ae9bca41e4d360ddba1b44821db4fc8b95
Will Smith <wsmith@linode.com>
2021-11-12 12:15:51 -0500

    bug: $refs are not resolved within schemas defined in allOfs
    I found this while working on https://github.com/linode/linode-cli/pull/218
    
    For a spec fragment like this:
    
    ```yaml
    schema:
      allOf:
        - $ref: '#/components/schemas/Example'
        - type: object
          properties:
            foo:
              $ref: '#/components/schemas/foo'
            bar:
              type: array
              items:
                $ref: '#/components/schemas/bar'
    ```
    
    neither of the `$ref`s in the second schema in the allOf will be
    resolved (the runtime will see each as a Reference type pointing to the
    correct part of the schema).

--- tests/conftest.py
+++ tests/conftest.py
@@ -136,3 +136,11 @@ def with_broken_links():
     Provides a spec with broken links defined
     """
     yield _get_parsed_yaml("with-broken-links.yaml")
+
+
+@pytest.fixture
+def with_nested_allof_ref():
+    """
+    Provides a spec with a $ref under a schema defined in an allOf
+    """
+    yield _get_parsed_yaml("nested-allOf.yaml")
